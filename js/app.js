0c2af10df21332b6cc1386decd37e4994c9785ab
Sam <mirthquakes@gmail.com>
2015-04-19 18:11:39 -0400

    first

---
+++ js/app.js
@@ -0,0 +1,10 @@
+var app = {
+    // Application Constructor
+    initialize: function() {
+        document.addEventListener('deviceready', function() {
+            console.log('are you ready too?');
+        }, false);
+    }
+};
+
+app.initialize();
