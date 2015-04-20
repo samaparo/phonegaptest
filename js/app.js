43871348d7ed76a605691897ef7e0b76819ee652
Sam <mirthquakes@gmail.com>
2015-04-19 20:13:58 -0400

    add navigation transitions. brute force page templates for demo

--- js/app.js
+++ js/app.js
@@ -4,18 +4,27 @@ var app = {
     	var initializeProcedure = (function(){
 
     		FastClick.attach(document.body);
-    		var demoLabel = document.getElementById('demo-label');
+    		var slider = new PageSlider($('.content'));
+
     		router.addRoute('coupon', function() {
-    			demoLabel.innerHTML = 'index';
+    			slider.slidePage($('<div><h1 class="demo-text">LATEST COUPON</h1></div>'));
+    			$('.title').html('Latest Coupon');
+    			$('.tab-item').removeClass('active');
+    			$('.tab-item').eq(0).addClass('active');
   			});
 
   			router.addRoute('locations', function() {
-  				demoLabel.innerHTML = 'locations';
-
+  				slider.slidePage($('<div><h1 class="demo-text">LOCATIONS</h1></div>'));
+  				$('.title').html('Store Locator');
+  				$('.tab-item').removeClass('active');
+    			$('.tab-item').eq(1).addClass('active');
   			});
 
   			router.addRoute('profile', function() {
-  				demoLabel.innerHTML = 'profile';
+  				slider.slidePage($('<div><h1 class="demo-text">PROFILE</h1></div>'));
+  				$('.title').html('Your Profile');
+  				$('.tab-item').removeClass('active');
+    			$('.tab-item').eq(2).addClass('active');
   			});
 
   			router.load('coupon');
